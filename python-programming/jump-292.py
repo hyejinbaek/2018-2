ff7fca7d9f1f4f05b28d2d48f96bad71a0047c43
hyejinbaek <bhj4208@cu.ac.kr>
2018-11-21 14:50:46 +0900

    add jump-292.py

--- python-programming/jump-292.py
+++ python-programming/jump-292.py
@@ -1,20 +1,15 @@
 #점프투파이썬 292쪽 문자열압축하기 문제
-strInput = "aaabbcccccca"
-stroutput = "a3b2c6a1"
-
-curChar = ""
-count = 0
-
-for i in strInput:
-    if curChar == i:
-        count = count + 1
+enter = input("문자열을 입력 : ")
+a = ''
+c=0
+for i in enter:
+    if(a==i):
+        c += 1
     else :
-        if count != 0:
-            print(curChar, i)
-            count = 1
+        if c != 0:
+            print(a + str(c) , end='')
+        a=i
+        c=0
+        c += 1
 
-    curChar = i
-
-print(curChar, i)
-
-print(curChar, count)
+print(a + str(c))
