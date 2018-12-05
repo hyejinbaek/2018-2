b81c7cfe0de06522aa0c30b52a4a8ce770c659ad
hyejinbaek <bhj4208@cu.ac.kr>
2018-12-05 16:44:03 +0900

    add 172.py

---
+++ python-programming/172.py
@@ -0,0 +1,11 @@
+#모두의 데이터과학 172쪽 문제 1번 스라소니 밀렵
+import pandas as pd
+
+lynx=pd.read_csv("lynx.csv")
+lynx["time"] = round(lynx.time / 10) * 10
+sum_lynx = lynx.groupby("time").sum()
+sort_lynx = sum_lynx.sort_values("time", ascending = False).head(10)
+print(sort_lynx)
+
+
+
